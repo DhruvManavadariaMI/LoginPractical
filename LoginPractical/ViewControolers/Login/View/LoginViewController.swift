4b9f902ce34009183bd5e939adcb85b6c6145cba
DhruvManavadariaMI <dhruv.manavdaria.mi@gmail.com>
2021-04-28 17:57:21 +0530

    - bind textfield with validation

--- LoginPractical/ViewControolers/Login/View/LoginViewController.swift
+++ LoginPractical/ViewControolers/Login/View/LoginViewController.swift
@@ -34,6 +34,7 @@ extension LoginViewController {
 	fileprivate func initialization() {
 		self.viewModel = LoginViewModel()
 		bindTextFields()
+		bindViewModel()
 	}
 	
 	private func bindTextFields() {
@@ -50,5 +51,58 @@ extension LoginViewController {
 				self?.viewModel.onPasswordChanged(text)
 			})
 			.disposed(by: disposeBag)
+	}
+	
+	private func bindViewModel() {
+
+		viewModel.showEmailErrorObservable
+			.takeUntil(rx.deallocating)
+			.bind(to: emailErrorLabel.rx.isHidden)
+			.disposed(by: disposeBag)
+
+		viewModel.showPasswordErrorObservable
+			.takeUntil(rx.deallocating)
+			.bind(to: passwordErrorLabel.rx.isHidden)
+			.disposed(by: disposeBag)
+
+		viewModel.enableLoginButtonObservable
+			.takeUntil(rx.deallocating)
+			.subscribe(onNext: { [weak self] (enable) in
+				self?.loginButton.isEnabled = enable
+				self?.loginButton.alpha = enable ? 1: 0.2
+			})
+			.disposed(by: disposeBag)
+
+		viewModel.showLoaderObservable
+			.takeUntil(rx.deallocating)
+			.subscribe(onNext: { [weak self] (loading) in
+				guard let self = self else { return }
+				loading ?
+					self.activityIndicator.startAnimating() :
+					self.activityIndicator.stopAnimating()
+				self.loginButton.isUserInteractionEnabled = !loading
+			})
+			.disposed(by: disposeBag)
+
+		viewModel.showHomePageObservable
+			.takeUntil(rx.deallocating)
+			.subscribe(onNext: { [weak self] (homePage) in
+			
+			})
+			.disposed(by: disposeBag)
+
+		viewModel.showLoginErrorObservable
+			.filter { (!($0 ?? "").isEmpty) }
+			.takeUntil(rx.deallocating)
+			.subscribe(onNext: { [weak self] (error) in
+				guard let self = self else { return }
+				UIAlertController
+					.present(in: self, title: AlertConstant.Error, message: error)
+					.subscribe(onNext: { buttonIndex in
+					}).disposed(by: self.disposeBag)
+
+			})
+			.disposed(by: disposeBag)
+
 	}
 }
--- LoginPractical/ViewControolers/Login/ViewModel/LoginViewModel.swift
+++ LoginPractical/ViewControolers/Login/ViewModel/LoginViewModel.swift
@@ -11,43 +11,65 @@ import RxCocoa
 
 final class LoginViewModel {
 
+	// MARK:- Variables -
 	private let disposeBag = DisposeBag()
+	
+	var showEmailErrorObservable: Observable<Bool> {
+		showEmailError.asObservable()
+	}
+	var showPasswordErrorObservable: Observable<Bool> {
+		showPasswordError.asObservable()
+	}
+	var enableLoginButtonObservable: Observable<Bool> {
+		enableLoginButton.asObservable()
+	}
+	var showLoginErrorObservable: Observable<String?> {
+		showLoginError.asObservable()
+	}
+	var showHomePageObservable: Observable<Bool> {
+		showHomePage.asObservable()
+	}
+	var showLoaderObservable: Observable<Bool> {
+		showLoader.asObservable()
+	}
 
-
-	// events
+	// MARK:- events -
 	private let email: BehaviorRelay<String> = BehaviorRelay(value: "")
 	private let password: BehaviorRelay<String> = BehaviorRelay(value: "")
 
 	// state
-
 	private let showEmailError = BehaviorRelay<Bool>(value: false)
 	private let showPasswordError = BehaviorRelay<Bool>(value: false)
 	private let enableLoginButton = BehaviorRelay<Bool>(value: false)
+	private let showLoader = BehaviorRelay<Bool>(value: false)
+	private let showLoginError = BehaviorRelay<String?>(value: nil)
+	private let showHomePage = BehaviorRelay<Bool>(value: false)
 
-
+	// MARK:- init -
 	init() {
 		bindState()
 	}
 
+	// MARK:- bindState -
 	private func bindState() {
-
+		
 		Observable.combineLatest(email.asObservable(), password.asObservable())
 			.map { (email, password) in
-				email.isEmpty && password.isEmpty
+				email.isValidEmail && password.isValidPassword
 			}
 			.bind(to: enableLoginButton)
 			.disposed(by: disposeBag)
 
 		email.asObservable()
 			.map {(email) in
-				email.isEmpty
+				email.isEmpty || email.isValidEmail
 			}
 			.bind(to: showEmailError)
 			.disposed(by: disposeBag)
 
 		password.asObservable()
 			.map {(password) in
-				password.isEmpty
+				password.isEmpty || password.isValidPassword
 			}
 			.bind(to: showPasswordError)
 			.disposed(by: disposeBag)
