7eb19a36d0667b5393e9d85370f490994cf2c9e8
DhruvManavadariaMI <dhruv.manavdaria.mi@gmail.com>
2021-04-28 16:54:49 +0530

    - integrate cocoapods dependency manager - Design login screen ui

---
+++ LoginPractical/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  LoginPractical
+//
+//  Created by mac-0004 on 28/04/21.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		initialization()
+	}
+}
+
+// MARK:- General Methods -
+extension LoginViewController {
+	fileprivate func initialization() {
+	
+	}
+}
